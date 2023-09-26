25e37d02ea4c0a4fd7b290d5c01f01c82a41feb6
younes-99 <93658714+younes-99@users.noreply.github.com>
2023-09-27 00:10:03 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import pandas as pd 
+import numpy as np
+import streamlit as st
+from google.cloud import bigquery
+import datetime
+import plotly.express as px
+st.set_page_config(layout="wide")
+import seaborn
+import matplotlib
+from millify import millify
+import scipy
+import plotly.figure_factory as ff
+import stats
+from functools import reduce
+from tabulate import tabulate
+import matplotlib.ticker as mtick
+from streamlit_extras.metric_cards import style_metric_cards
+from stats_engine import mvt_bayesian_calculator_manual
+from streamlit_ace import st_ace
+from scipy.stats import chisquare
+dojo_colours = ('#00A699','#FF4655','#FFB100','#860053','#26D07C','#7084FF','#262626')
+seaborn.set_palette(seaborn.color_palette(dojo_colours))
+
+
+
+
+st.markdown('\n')
+st.markdown('\n')
+mi_variants = st.number_input('Insert Number of Variation',value = 2,step = 1)
+manual_input_columns = st.columns(2) 
+mi_sample_size_dict = {}
+mi_sample_conversion_dict = {}
+    # mi_sample_size_dict[variant] = 
+    # mi_sample_conversion_dict[variant] = 
+with manual_input_columns[0]:
+    for variant in range(0,mi_variants):
+        if variant == 0:
+            mi_sample_size_dict[variant] = st.number_input(f"Sample Size for Control",step =1,value  = 500)
+        else:
+            mi_sample_size_dict[variant] = st.number_input(f"Sample Size for Variant {variant}",step = 1,value = 500)
+
+with manual_input_columns[1]:
+    for variant in range(0,mi_variants):
+        if variant == 0 :
+            mi_sample_conversion_dict[variant] = st.number_input(f"Conversion for Control",step = 1,value = 200)
+        else:
+            mi_sample_conversion_dict[variant] = st.number_input(f"Conversion for Variant {variant}",step = 1, value = 200)
+expected_loss_type = st.radio('Expected loss type', ['Relative (%)','Absolute (%)'],horizontal=True)
+expected_loss_threshold = st.number_input('Expected loss threshold',min_value=0.0,format='%f',value=2.0)
+# if show_sql == 'Yes':                
+    # code = f'''PUT file://<file_path>/<file_name> @{st.session_state['table']}/ \n\nCOPY INTO "SANDBOX"."DATA_LOADING"."{st.session_state['table']}" FROM @/ui1675186369009\nON_ERROR = 'SKIP_FILE_0' PURGE = TRUE;'''
+    # st.code(code, language='plsql')  
+
+
+if st.button("Submit"):
+    analysis = mvt_bayesian_calculator_manual(mi_sample_size_dict,mi_sample_conversion_dict,expected_loss_threshold,expected_loss_type)
+    analysis.get_probablities()
+    st.write("")
+    analysis.plotting_graph()
---
+++ stats_engine.py
@@ -0,0 +1,121 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy
+import stats
+from functools import reduce
+from tabulate import tabulate
+import matplotlib.ticker as mtick
+import matplotlib.patches as mpatches
+from scipy.stats import norm
+import streamlit as st
+import altair as alt
+import math
+
+
+class mvt_bayesian_calculator_manual:
+    
+    def __init__(self,sample_size_dict,conversions_dict,expected_loss_threshold,expected_loss_type,
+                      mcs_simulations=10000):
+
+
+            self.sample_size_dict = sample_size_dict
+            self.conversions_dict = conversions_dict
+            self.expected_loss_threshold = expected_loss_threshold
+            self.expected_loss_type = expected_loss_type
+            self.mcs_simulations = mcs_simulations
+            self.alpha_dict = {}
+            self.beta_dict = {}
+            self.values_dict = {}
+            self.categories_wins = {}
+            self.prob_wins_dict={}
+            for variant in self.sample_size_dict.keys():
+                self.alpha_dict [variant] = 1 + self.conversions_dict[variant]
+                self.beta_dict [variant] = 1 + self.sample_size_dict[variant] - self.conversions_dict[variant]
+                self.values_dict[variant] = np.random.beta(self.alpha_dict[variant],self.beta_dict[variant],mcs_simulations)
+                self.categories_wins[variant] = 0
+    
+    def get_probablities(self):
+        for sample in range(self.mcs_simulations):
+            values_samples = {}
+            for variant in self.sample_size_dict.keys():
+                values_samples[variant] = self.values_dict[variant][sample]
+            
+            winner = max(values_samples,key=values_samples.get)
+            print(sample,winner)
+            self.categories_wins[winner]+=1
+
+        for variant in self.sample_size_dict.keys():
+            self.prob_wins_dict[variant] = round((self.categories_wins[variant]/self.mcs_simulations)*100,3)
+
+        return self.prob_wins_dict
+
+    def get_expected_loss(self):
+        # this logic is coming from the following VWO paper, page 14: https://vwo.com/downloads/VWO_SmartStats_technical_whitepaper.pdf
+        for variant in self.list_of_variants:
+
+            list_of_values = [self.values_dict[variant]]
+
+            all_other_variants_list = [x for x in self.list_of_variants if x != variant]
+
+            for other_variant in all_other_variants_list:
+                list_of_values.append(self.values_dict[other_variant])
+
+
+            
+            samples =list(zip(*list_of_values))
+
+
+            # for other_variant in all_other_variants_list:
+
+            # print(other_variant,variant)
+                # samples =list(zip(values_dict[other_variant],values_dict[variant]))
+                # print(samples)
+            # for idx in enumerate():
+            # print(samples)
+            # difference = map(lambda sample: np.max([sample[0]-sample[1], 0]), samples)
+
+            # differences = list(map(lambda sample: np.max([sample[0] - sample[i] for i in range(1, len(sample)),0]), samples))
+
+            # differences = list(map(lambda sample: np.max([sample[0] - sample[i], 0 for i in range(1, len(sample))]), samples))
+            differences_for_variant = map(lambda sample: np.max([(sample[0] - sample[i],0) for i in range(1, len(sample))]), samples)
+            # print(list(differences_for_variant))
+            sum_diff_variant = reduce(lambda x,y:x+y, differences_for_variant)
+
+            # print(sum_diff_variant.mean())
+
+            absolute_expected_loss_variant = (sum_diff_variant/self.mcs_simulations) * 100
+
+            relative_expected_loss_variant = absolute_expected_loss_variant/self.conversions_dict[variant]
+
+            print("absolute_expected_loss_variant",variant,"is ",absolute_expected_loss_variant)
+            # print(sum_diff_variant.mean())
+                
+            print("relative_expected_loss_variant",variant,"is ",relative_expected_loss_variant)
+            # break
+    def plotting_graph(self):
+
+        # st.write(prob_wins_dict)
+        
+        prob_wins_dict_fig = {"Variation":list(self.prob_wins_dict.keys()),"Probability":list(self.prob_wins_dict.values())}
+        # st.write(prob_wins_dict_fig)
+        # # print(prob_wins_dict_fig)
+        # fig = px.bar(prob_wins_dict_fig, x='Probability', y='Variation',orientation='h',title='Probability to be Best',opacity=1,text = "Probability",width= 1000,height = 500)
+        # fig.update_yaxes(showgrid=True)
+        # st.plotly_chart(fig,use_container_width=True)
+        source = pd.DataFrame(prob_wins_dict_fig)
+        bars = alt.Chart(source).mark_bar().encode(
+            x='Probability:Q',
+            y="Variation:O"
+        ).properties(title = "Probability to be Best ")
+        text = bars.mark_text(
+                # align='center',
+                # baseline='middle',
+                xOffset = -50,
+                dx=-25 # Nudges text to right so it doesn't appear on top of the bar
+            ).encode(
+                text='Probability:Q'
+            )
+        chart = (bars + text).properties(width= 1000,height = 500)
+        chart = chart.configure_axis(grid=False)
+        st.altair_chart(chart, theme="streamlit", use_container_width=True)
